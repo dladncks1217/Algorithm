902c41108e3a16101c62cada5dbee2fb0263c3f2
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-03-14 01:50:28 +0900

    0314

---
+++ Node.js/2558.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var input = fs.readFileSync('/dev/stdin').toString().split('\n');
+var i = parseInt(input[100]);
+var trys = 0;
+for(;trys<=i;trys++){
+    var a = parseInt(input[0]).split(' ');
+    console.log(a+b);
+}
