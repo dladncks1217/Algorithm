4787a2fc5ca16c9327ccd46cd66b116aeff72bdf
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-09-12 21:07:03 +0900

    0912

---
+++ Java/15740.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.math.BigDecimal;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		BigDecimal a = scan.nextBigDecimal();
+		BigDecimal b = scan.nextBigDecimal();
+		System.out.println(a.add(b));
+	}
+}
