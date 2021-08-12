6812fbfbbc3144d9a0043258a8b61938e1a34056
dladncks1217 <dlaxodud1217@gmail.com>
2021-08-12 21:14:18 +0900

    0812

---
+++ programmers/최댓값과최솟값.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    var answer = [];
+    let arr = s.split(" ");
+    answer.push(Math.min.apply("",arr))
+    answer.push(Math.max.apply("",arr));
+    return `${answer[0]} ${answer[1]}`;
+}
+console.log(solution("-1 -2 -3 -4"));
