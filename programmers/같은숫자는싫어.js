b92793d0614ebe902cfa36f1962ba5ff2074ddc6
dladncks1217 <dlaxodud1217@gmail.com>
2021-08-22 23:24:48 +0900

    0822

---
+++ programmers/같은숫자는싫어.js
@@ -0,0 +1,8 @@
+function solution(arr){
+    let answer = [];
+    answer.push(arr[0]);
+    for(let i = 1;i<arr.length;i++){
+        if(arr[i-1]!==arr[i]) answer.push(arr[i])
+    }
+    return answer;
+}
