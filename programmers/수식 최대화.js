848dca7411b5dca08f4ce97f982574322763f99f
dladncks1217 <dlaxodud1217@gmail.com>
2021-10-07 22:32:52 +0900

    1007

--- programmers/수식 최대화.js
+++ programmers/수식 최대화.js
@@ -37,6 +37,33 @@ function solution(expression) {
   }
   console.log(numoper);
 
+  prior.forEach((v) => {
+    const temp = numoper.concat();
+
+    v.forEach((oper) => {
+      while (temp.includes(oper)) {
+        const curr = temp.indexOf(oper);
+        const prev = temp[curr - 1];
+        const next = temp[curr + 1];
+
+        let result;
+        switch (oper) {
+          case "*":
+            result = +prev * +next;
+            break;
+          case "+":
+            result = +prev + +next;
+            break;
+          case "-":
+            result = +prev - +next;
+            break;
+        }
+        temp.splice(curr - 1, 3, result.toString());
+      }
+    });
+    answer = Math.max(answer, Math.abs(temp[0]));
+  });
+
   return answer;
 }
 
