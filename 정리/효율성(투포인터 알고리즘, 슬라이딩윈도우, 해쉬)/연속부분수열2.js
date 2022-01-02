71e064ce2685e2020a68b93f17ab58c87c06a2bd
dladncks1217 <dlaxodud1217@gmail.com>
2022-01-02 23:04:51 +0900

    210102

---
+++ 정리/효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/연속부분수열2.js
@@ -0,0 +1,16 @@
+function solution(m, arr) {
+  let answer = 0,
+    sum = 0,
+    lt = 0;
+  for (let rt = 0; rt < arr.length; rt++) {
+    sum += arr[rt];
+    while (sum > m) {
+      sum -= arr[lt++];
+    }
+    answer += rt - lt + 1;
+  }
+  return answer;
+}
+
+let a = [1, 3, 1, 2, 3];
+console.log(solution(5, a));
