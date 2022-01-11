e176a479640a6a48958e11f02fa2292c74bef8c7
dladncks1217 <dlaxodud1217@gmail.com>
2022-01-11 15:36:42 +0900

    220111

---
+++ programmers/카펫.js
@@ -0,0 +1,14 @@
+function solution(brown, yellow) {
+  let answer = [];
+  let all = brown + yellow;
+  for (let i = 3; i < brown; i++) {
+    if (all % i === 0) {
+      answer[0] = i;
+      answer[1] = all / i;
+    }
+    if (i + i + (answer[1] - 2) * 2 === brown) {
+      answer.sort((a, b) => b - a);
+      return answer;
+    }
+  }
+}
