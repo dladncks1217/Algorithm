13de8e851c7d6863eee299649acf79cf92eb92fe
dladncks1217 <dlaxodud1217@gmail.com>
2022-03-21 02:56:43 +0900

    220321

---
+++ programmers/2개 이하로 다른 비트.js
@@ -0,0 +1,31 @@
+function changebit(bit) {
+  bit = bit.split("");
+  bit.unshift("0");
+  for (let i = bit.length - 1; i >= 0; i--) {
+    if (bit[bit.length - 1] === "0") {
+      bit[i] = "1";
+      break;
+    } else {
+      if (bit[i] === "0") {
+        bit[i] = "1";
+        bit[i + 1] = "0";
+        break;
+      }
+    }
+  }
+
+  bit = bit.join("");
+  return Number(BigInt(parseInt(bit, 2)));
+}
+
+function solution(numbers) {
+  let answer = [];
+  numbers.forEach((v) => {
+    const binary = v.toString(2);
+    answer.push(changebit(binary));
+  });
+
+  return answer;
+}
+
+console.log(solution([2, 7]));
