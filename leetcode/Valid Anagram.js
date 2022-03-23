d85afa03354bd649eba4afa5e8d81edc6b04e572
dladncks1217 <dlaxodud1217@gmail.com>
2022-03-23 20:05:41 +0900

    220323

---
+++ leetcode/Valid Anagram.js
@@ -0,0 +1,26 @@
+let isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  let answer = true;
+  let hash = {};
+  s = s.split("");
+  t = t.split("");
+  s.forEach((v) => {
+    if (hash.hasOwnProperty(v)) {
+      hash[v] += 1;
+    } else {
+      hash[v] = 1;
+    }
+  });
+  //   console.log(hash);
+  t.forEach((v) => {
+    if (hash.hasOwnProperty(v)) {
+      if (hash[v] === 0) return (answer = false);
+      hash[v] -= 1;
+    } else {
+      return (answer = false);
+    }
+  });
+  //   console.log(hash);
+  return answer;
+};
+console.log(isAnagram("rat", "car"));
