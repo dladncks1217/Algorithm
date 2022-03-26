5d7eb19d24d2bd77774e6b6734349860e6ed9047
dladncks1217 <dlaxodud1217@gmail.com>
2022-03-26 20:59:30 +0900

    220326

---
+++ 정리/정렬과 그리디, 결정알고리즘(이분검색)/선택정렬복습.js
@@ -0,0 +1,14 @@
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[idx]) idx = j;
+    }
+    [arr[i], arr[idx]] = [arr[idx], arr[i]];
+  }
+
+  return answer;
+}
+
+console.log(solution([13, 5, 11, 7, 23, 15]));
