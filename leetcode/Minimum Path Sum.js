6e252009b9d7ed4449a083753eb8d17ba60f8594
dladncks1217 <dlaxodud1217@gmail.com>
2022-04-10 15:39:59 +0900

    220410

--- leetcode/Minimum Path Sum.js
+++ leetcode/Minimum Path Sum.js
@@ -5,12 +5,30 @@
  */
 let minPathSum = function (grid) {
   let answer = 0;
+
+  // 세로
   for (let i = 0; i < grid.length; i++) {
-    for (let k = 0; k < grid[0].length; k++) {
-      // if()
+    if (i !== 0) {
+      grid[i][0] += grid[i - 1][0];
     }
   }
-  return answer;
+
+  // 가로
+  for (let i = 0; i < grid[0].length; i++) {
+    if (i !== 0) {
+      grid[0][i] += grid[0][i - 1];
+    }
+  }
+  for (let i = 0; i < grid.length; i++) {
+    for (let k = 0; k < grid[0].length; k++) {
+      if (i !== 0 && k !== 0) {
+        let check = grid[i][k];
+        grid[i][k] = Math.min(check + grid[i - 1][k], check + grid[i][k - 1]);
+      }
+    }
+  }
+
+  return grid[grid.length - 1][grid[0].length - 1];
 };
 
 console.log(
