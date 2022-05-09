ac575b73702784d434cad0afa13b664cf1eaa278
dladncks1217 <dlaxodud1217@gmail.com>
2022-05-09 22:30:49 +0900

    220509

---
+++ 정리/정렬과 그리디, 결정알고리즘(이분검색)/좌표정렬복습.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    else return a[0] - b[0];
+  });
+  return arr;
+}
+
+console.log(
+  solution([
+    [2, 7],
+    [1, 3],
+    [1, 2],
+    [2, 5],
+    [3, 6],
+  ])
+);
