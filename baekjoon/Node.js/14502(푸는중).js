ead34227e512d7d3daeeeb0dfd3a695263da31f5
dladncks1217 <dlaxodud1217@gmail.com>
2022-07-10 03:08:46 +0900

    220710

--- baekjoon/Node.js/14502(푸는중).js
+++ baekjoon/Node.js/14502(푸는중).js
@@ -8,15 +8,54 @@ const [data, ...input] = require("fs")
     return v;
   });
 
+let answer = 0;
+
 const dx = [1, 0, -1, 0];
 const dy = [0, -1, 0, 1];
 
 const [col, row] = [data[0], data[1]]; // col, row
 let check = Array.from(Array(col), () => Array(row).fill(0)); // 간곳 또 안가게
 
+let walls = []; // 벽 가능목록
+
+for (let y1 = 0; y1 < col; y1++) {
+  for (let x1 = 0; x1 < row; x1++) {
+    let temp = [];
+    if (input[y1][x1] === 0) {
+      temp.push(x1, y1);
+
+      for (let y2 = y1; y2 < col; y2++) {
+        for (let x2 = x1; x2 < row; x2++) {
+          if (y1 === y2 && x1 === x2) continue;
+          if (input[y2][x2] === 0) {
+            temp.push(x2, y2);
+
+            for (let y3 = y2; y3 < col; y3++) {
+              for (let x3 = x2; x3 < row; x3++) {
+                if ((y1 === y3 && x1 === x3) || (y2 === y3 && x2 === x3))
+                  continue;
+                if (input[y3][x3] === 0) {
+                  temp.push(x3, y3);
+                  walls.push(temp.slice());
+                  temp.pop();
+                  temp.pop();
+                }
+              }
+            }
+            temp.pop();
+            temp.pop();
+          }
+        }
+      }
+      temp.pop();
+      temp.pop();
+    }
+  }
+}
+
 // 그래프 탐색
-function DFS(x, y) {
-  if (y === col && x === row) {
+function DFS(x, y, graph) {
+  if (y === col - 1 && x === row - 1) {
     return;
   } else {
     for (let i = 0; i < 4; i++) {
@@ -24,8 +63,8 @@ function DFS(x, y) {
       if (nx >= 0 && ny >= 0 && nx < row && ny < col && check[ny][nx] === 0) {
         if (input[ny][nx] === 0) {
           check[ny][nx] = 1;
-          input[ny][nx] = 3; // 바이러스 체크된거 3으로
-          DFS(nx, ny);
+          graph[ny][nx] = 3; // 바이러스 체크된거 3으로
+          DFS(nx, ny, graph);
           check[ny][nx] = 0;
         }
       }
@@ -33,15 +72,35 @@ function DFS(x, y) {
   }
 }
 
+let virus = []; // 바이러스 위치들
 for (let i = 0; i < col; i++) {
   for (let k = 0; k < row; k++) {
     if (input[i][k] === 2) {
-      input[i][k] = 3; // 바이러스 체크된거 3으로
-      DFS(k, i);
+      virus.push([k, i]);
     }
   }
 }
 
-console.log(input);
+walls.forEach((v) => {
+  let tempGraph = input.map((v) => {
+    return v.slice();
+  });
+  tempGraph[v[1]][v[0]] = 1;
+  tempGraph[v[3]][v[2]] = 1;
+  tempGraph[v[5]][v[4]] = 1;
+  for (let i = 0; i < virus.length; i++) {
+    // tempGraph[virus[i][1]][virus[i][0]] = 3; // 바이러스 체크된거 3으로
+    DFS(virus[i][0], virus[i][1], tempGraph);
+  }
+  let count = 0;
+  tempGraph.forEach((v) => {
+    v.forEach((k) => {
+      if (k === 0) count++;
+    });
+  });
+  answer = Math.max(answer, count);
+  console.log(tempGraph);
+});
 
+console.log(answer);
 // 벽 3개 다 완탐해야될듯
