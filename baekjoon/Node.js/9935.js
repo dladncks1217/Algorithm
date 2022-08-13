7999dd783125624f0aae3d5fee9823b9ac6fa8bf
dladncks1217 <dlaxodud1217@gmail.com>
2022-08-13 20:00:35 +0900

    220813

---
+++ baekjoon/Node.js/9935.js
@@ -0,0 +1,25 @@
+const [str, bomb] = require("fs")
+  .readFileSync("./dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let stack = [];
+let searchStart = bomb[bomb.length - 1];
+let top = 0;
+
+for (let i = 0; i < str.length; i++) {
+  stack.push(str[i]);
+  top = str[i];
+
+  if (top === searchStart) {
+    let last = stack.slice(-bomb.length);
+    let checkstr = last.join("");
+
+    if (bomb === checkstr) {
+      stack.splice(-bomb.length);
+    }
+  }
+}
+
+stack.length > 0 ? console.log(stack.join("")) : console.log("FRULA");
